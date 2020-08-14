2e620da891d12378fac63622923f335c38dae300
Aaronmb123 <bakeaaro@oregonstate.edu>
2020-08-14 14:44:29 -0700

    added AndroidEntryPoint to ExampleActivity, renamed MainActivity

--- app/src/main/java/com/hilt/ExampleActivity.kt
+++ app/src/main/java/com/hilt/ExampleActivity.kt
@@ -2,8 +2,10 @@ package com.hilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class ExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
